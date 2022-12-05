2d473d64e67d09f9f96b339616b5e8f0cedb597f
Noel345 <58745415+Noel3454565FNF@users.noreply.github.com>
2022-12-05 16:01:57 +0100

    version 1.0V du script score-see

--- script/score-see.lua
+++ script/score-see.lua
@@ -9,17 +9,55 @@ function onCreate()
 end
 
 local ScoreADD = 0;
+local ScoreColour = -1;
 
 function goodNoteHit(membersIndex, noteData, noteType, isSustainNote)
     if noteType == '' then
         ScoreADD = ScoreADD + 100;
+        ScoreColour = ScoreColour + 1;
         setTextString('score', getTextString('score') + 100);
     end
+end
+
+function onUpdate(elapsed)
+    if ScoreColour == 0 then
+        setTextColor('score', 1);
+
+    elseif ScoreColour == 1 then
+            setTextColor('score', 11);
+            
+    elseif ScoreColour == 2 then
+            setTextColor('score', 1000);
+
+    elseif ScoreColour == 3 then
+            setTextColor('score', 18000);
+
+    elseif ScoreColour == 4 then
+        setTextColor('score', 36000);
+
+    elseif ScoreColour == 5 then
+        setTextColor('score', 72000);
+
+    elseif ScoreColour == 6 then
+        setTextColor('score', 144000);
+
+    elseif ScoreColour == 7 then
+        setTextColor('score', 288000);
+
+    elseif ScoreColour == 8 then
+        setTextColor('score', 576000);
+
+    elseif ScoreColour >= 9 then
+        ScoreColour = 0;
+
 end
 
 function noteMiss(membersIndex, noteData, noteType, isSustainNote)
     if noteType == '' then
         ScoreADD = ScoreADD - 100;
+        ScoreColour = ScoreColour - 1;
         setTextString('score', getTextString('score') - 100);
     end
+end
+
 end
